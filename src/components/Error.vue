52030c547ba9b39555e38d46503b2f3005532317
Radek <radek511@op.pl>
2018-05-29 02:29:42 +0200

    Edit - accessibility for link

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -2,7 +2,7 @@
   <main id="page-not-found">
     <h1 class="page-not-found-header">Oh no, page not found...</h1>
     <p class="page-not-found-error">404 error</p>
-    <a @click="$router.go(-1)" class="page-not-found-link">Back</a>
+    <a @click="$router.go(-1)" class="page-not-found-link" aria-label="Back " title="Back">Back</a>
   </main>
 </template>
 
