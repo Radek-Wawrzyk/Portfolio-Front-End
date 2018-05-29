8acd7be4c256d3ccbb10417eb8102b1576cc152e
Radek <radek511@op.pl>
2018-05-29 02:27:53 +0200

    Add - Error component with styles
    404 page not found component

---
+++ src/components/Error.vue
@@ -0,0 +1,15 @@
+<template>
+  <main id="page-not-found">
+    <h1 class="page-not-found-header">Oh no, page not found...</h1>
+    <p class="page-not-found-error">404 error</p>
+    <a @click="$router.go(-1)" class="page-not-found-link">Back</a>
+  </main>
+</template>
+
+<script>
+  
+  export default {
+    name: "Error"
+  }
+
+</script>
