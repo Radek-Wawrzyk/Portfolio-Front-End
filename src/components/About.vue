ae3c667af08972dda0cf0b14cd0e7695d743ee77
Radek <radek511@op.pl>
2018-06-22 12:43:12 +0200

    Refactor - About component with styles
    Also compress & resize image

--- src/components/About.vue
+++ src/components/About.vue
@@ -20,7 +20,7 @@
         <div class="about-info-wrapper">
           <div class="about-info-tile"></div>
           <div class="about-info-tile">
-            <a href="https://drive.google.com/open?id=1fWsafCvqLPb2LHXScqeMsW9H1q0jOTeb" aria-label="Check my CV" title="My CV">
+            <a href="https://drive.google.com/open?id=1fWsafCvqLPb2LHXScqeMsW9H1q0jOTeb" target="_blank" aria-label="Check my CV" title="My CV">
               <h2 class="about-info-tile-title">
                 Curriculum Vitae
               </h2>
@@ -98,6 +98,7 @@
           </p>
           <router-link to="/portfolio" title="my work" aria-label="My work" class="about-exp-info-link">
             See my work
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M313.941 216H12c-6.627 0-12 5.373-12 12v56c0 6.627 5.373 12 12 12h301.941v46.059c0 21.382 25.851 32.09 40.971 16.971l86.059-86.059c9.373-9.373 9.373-24.569 0-33.941l-86.059-86.059c-15.119-15.119-40.971-4.411-40.971 16.971V216z"/></svg>
           </router-link>
         </article>
       </section>
