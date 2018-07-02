de8c9cc00a785395b8411ee696df467c951ec960
Radek <radek511@op.pl>
2018-07-02 23:22:44 +0200

    Edit - Footer text

--- src/Components/Footer.vue
+++ src/Components/Footer.vue
@@ -3,7 +3,7 @@
     <header class="footer-header">
       <h4 class="footer-header-title">Quick contact</h4>
       <p class="footer-header-description">
-        You can easily contact with me via email, phone call or Facebook message.<br/>
+        You can easily contact me via e-mail, phone call or messenger. <br/>
         Feel free to ask! 
       </p>
       <span class="footer-header-line"></span>
@@ -14,7 +14,7 @@
           <div class="footer-contact-item-icon">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M440 6.5L24 246.4c-34.4 19.9-31.1 70.8 5.7 85.9L144 379.6V464c0 46.4 59.2 65.5 86.6 28.6l43.8-59.1 111.9 46.2c5.9 2.4 12.1 3.6 18.3 3.6 8.2 0 16.3-2.1 23.6-6.2 12.8-7.2 21.6-20 23.9-34.5l59.4-387.2c6.1-40.1-36.9-68.8-71.5-48.9zM192 464v-64.6l36.6 15.1L192 464zm212.6-28.7l-153.8-63.5L391 169.5c10.7-15.5-9.5-33.5-23.7-21.2L155.8 332.6 48 288 464 48l-59.4 387.3z"/></svg>
           </div>
-          <p class="footer-contact-item-addition">Write to me</p>
+          <p class="footer-contact-item-addition">Write me an e-mail</p>
           <p class="footer-contact-item-detail">radek511@op.pl</p>
         </a>
       </li>
@@ -32,7 +32,7 @@
          <div class="footer-contact-item-icon">
            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 32C114.6 32 0 125.1 0 240c0 47.6 19.9 91.2 52.9 126.3C38 405.7 7 439.1 6.5 439.5c-6.6 7-8.4 17.2-4.6 26S14.4 480 24 480c61.5 0 110-25.7 139.1-46.3C192 442.8 223.2 448 256 448c141.4 0 256-93.1 256-208S397.4 32 256 32zm0 368c-26.7 0-53.1-4.1-78.4-12.1l-22.7-7.2-19.5 13.8c-14.3 10.1-33.9 21.4-57.5 29 7.3-12.1 14.4-25.7 19.9-40.2l10.6-28.1-20.6-21.8C69.7 314.1 48 282.2 48 240c0-88.2 93.3-160 208-160s208 71.8 208 160-93.3 160-208 160z"/></svg>
          </div>
-          <p class="footer-contact-item-addition">Talk with me</p>
+          <p class="footer-contact-item-addition">Chat with me</p>
           <p class="footer-contact-item-detail">Radek Wawrzyk</p>
         </a>
       </li>
