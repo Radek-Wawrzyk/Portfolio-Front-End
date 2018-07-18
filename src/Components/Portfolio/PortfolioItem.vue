3f6c0aa6e30d82281280d2e8c9fe4f56858607de
Radek <radek511@op.pl>
2018-07-18 21:10:25 +0200

    Fix - Shop items trasition/animation
    Also edit Data object with portfolio websites

--- src/Components/Portfolio/PortfolioItem.vue
+++ src/Components/Portfolio/PortfolioItem.vue
@@ -1,15 +1,13 @@
 <template>
-  <transition name="fade-up">
-    <li class="portfolio-list-item">
-      <router-link v-bind:to="'portfolio/'+portfolioItem.link">
-        <article class="portfolio-list-item-description">
-          <p class="portfolio-list-item-text">{{portfolioItem.title}}</p>
-          <p class="portfolio-list-item-text">{{portfolioItem.description}}</p>
-        </article>
-        <img class="portfolio-list-item-img" v-bind:src="portfolioItem.img" :alt="portfolioItem.title" />
-      </router-link>
-    </li>
-  </transition>
+  <li class="portfolio-list-item">
+    <router-link v-bind:to="'portfolio/'+portfolioItem.link">
+      <article class="portfolio-list-item-description">
+        <p class="portfolio-list-item-text">{{portfolioItem.title}}</p>
+        <p class="portfolio-list-item-text">{{portfolioItem.description}}</p>
+      </article>
+      <img class="portfolio-list-item-img" v-bind:src="portfolioItem.img" :alt="portfolioItem.title" />
+    </router-link>
+  </li>
 </template>
 
 <script>
--- src/Data/data.js
+++ src/Data/data.js
@@ -1,34 +1,98 @@
 export const Data = [
   {
     id: 1,
-    link: "forecast-app",
-    title: "Forecast app",
-    description: "Simply forecast appliaction based on API",
+    link: "vue-todolist",
+    title: "Vue TodoList",
+    header: "ToDoList written on vue.js, where you can add your own tasks. ",
+    description: "Basically, simple to-do-list written on vue.js - you can easily add important things that you have to do in the future.",
     img: "http://www.wallpapers4u.org/wp-content/uploads/puppy_couple_sunset_nature_play_kids_1194_1920x1080.jpg",
-    category: "app"
+    category: "app",
+    technologies: [
+      "HTML5", "CSS3", "Javascript", "Vue.js", "Vuex", "Vue router"
+    ]
   },
   {
     id: 2,
-    link: "bossbuss",
-    title: "Bossbuss",
-    description: "Site about awesome buses",
-    img: "https://wallpaperscraft.com/image/bali_indonesia_babadan_beach_canggu_106056_1920x1080.jpg",
-    category: "website"
+    link: "vue-shop",
+    title: "Vue Shop",
+    header: "Shop aplication based on Vue.js, REST API & Laravel",
+    description: "Shop aplication based on Vue.js, REST API & Laravel",
+    img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
+    category: "app",
+    technologies: [
+      "HTML5", "CSS3", "SASS", "Javascript", "ES6", "Vue.js", "Vuex", "Vue router", "JWT", "REST API", "Webpack",
+    ]
   },
   {
     id: 3,
-    link: "watch-shop",
-    title: "Watch Shop",
-    description: "Online shop on React and Laravel",
-    img: "https://wallpaperscraft.com/image/mountains_sky_bali_sunrise_kintamani_indonesia_95497_1920x1080.jpg",
-    category: "website"
+    link: "bossbus",
+    title: "Bossbus",
+    header: "Website of a company which rents cars for specified amounts of time",
+    description: "Website had to be simple, but effective. In cooperation with my graphic designer, we have created modern, simple and good looking website perfect for our client and his business. Cars are the main target which is very visible for every client.",
+    img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
+    category: "website",
+    technologies: [
+      "HTML5", "CSS3", "LESS", "Javascript", "jQuery", "Owl Carousel", "AOS.js"
+    ]
   },
   {
     id: 4,
-    link: "shit-site",
-    title: "Shit site",
-    description: "I don't know, it's shitty site",
+    link: "sokolnicki-agency",
+    title: "Sokolnicki agency",
+    header: "Website for Sokolnicki and his services",
+    description: "It is fully written by me from a PSD file delivered by his designer. I assisted with a designer in some changes in design. A website is fast and very modern.",
+    img: "https://wallpaperscraft.com/image/mountains_sky_bali_sunrise_kintamani_indonesia_95497_1920x1080.jpg",
+    category: "website",
+    technologies: [
+      "HTML5", "CSS3", "SASS", "Javascript", "ES6", "Gulp"
+    ]
+  },
+  {
+    id: 5,
+    link: "radek-wawrzyk",
+    title: "Radek Wawrzyk Portfolio",
+    header: "Radek Wawrzyk Portfolio",
+    description: "My personal website ",
     img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
-    category: "app"
+    category: "app",
+    technologies: [
+      "HTML5", "CSS3", "SASS", "Javascript", "ES6", "Vue.js", "Vuex", "Vue router", "Webpack", "Firebase"
+    ]
+  },
+  {
+    id: 6,
+    link: "forecast",
+    title: "Forecast",
+    header: "Simple application which shows you actually weather",
+    description: "Application which shows you weather based on your localisation.",
+    img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
+    category: "app",
+    technologies: [
+      "HTML5", "CSS3", "Javascript", "ES6", "React.js", "API"
+    ]
+  },
+  {
+    id: 7,
+    link: "mp-transporte",
+    title: "MP Transporte",
+    header: "Rework of existing service ",
+    description: "Design rework of existing service for internet fake-company in-game ETS2.",
+    img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
+    category: "website",
+    technologies: [
+      "HTML5", "CSS3", "LESS", "Javascript", "jQuery"
+    ]
+  },
+  {
+    id: 8,
+    link: "cersoir",
+    title: "Cersoir",
+    header: "Page based on PSD file",
+    description: "Page based on PSD file",
+    img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
+    category: "app",
+    technologies: [
+      "HTML5", "CSS3", "SASS", "Javascript", "ES6", "Gulp"
+    ]
   }
 ];
