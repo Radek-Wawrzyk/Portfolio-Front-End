217a98ae4af60f1fddece5c33d1082e623f0e190
Radek <radek511@op.pl>
2018-07-19 20:19:36 +0200

    Edit - Data file
    Change categories & technologies fields

--- src/Data/data.js
+++ src/Data/data.js
@@ -6,9 +6,9 @@ export const data = [
     header: "ToDoList written on vue.js, where you can add your own tasks. ",
     description: "Basically, simple to-do-list written on vue.js - you can easily add important things that you have to do in the future.",
     img: "http://www.wallpapers4u.org/wp-content/uploads/puppy_couple_sunset_nature_play_kids_1194_1920x1080.jpg",
-    category: "app",
+    category: "Application",
     technologies: [
-      "HTML5", "CSS3", "Javascript", "Vue.js", "Vuex", "Vue router"
+      "HTML5/CSS3", "Javascript/ES6", "Vue.js + Vue-Router", "Vuex state management"
     ]
   },
   {
@@ -18,9 +18,9 @@ export const data = [
     header: "Shop aplication based on Vue.js, REST API & Laravel",
     description: "Shop aplication based on Vue.js, REST API & Laravel",
     img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
-    category: "app",
+    category: "Application",
     technologies: [
-      "HTML5", "CSS3", "SASS", "Javascript", "ES6", "Vue.js", "Vuex", "Vue router", "JWT", "REST API", "Webpack",
+      "HTML5/CSS3", "Javascript/ES6", "SCSS preprocessor", "Vue.js framework + Vue-Router", "Vuex state management", "JWT authorization", "REST API architecture", "Webpack module bundler",
     ]
   },
   {
@@ -30,33 +30,33 @@ export const data = [
     header: "Website of a company which rents cars for specified amounts of time",
     description: "Website had to be simple, but effective. In cooperation with my graphic designer, we have created modern, simple and good looking website perfect for our client and his business. Cars are the main target which is very visible for every client.",
     img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
-    category: "website",
+    category: "Website",
     technologies: [
-      "HTML5", "CSS3", "LESS", "Javascript", "jQuery", "Owl Carousel", "AOS.js"
+      "HTML5/CSS3", "Javascript/jQuery", "LESS preprocessor", "Owl Carousel library", "AOS.js animation library"
     ]
   },
   {
     id: 4,
     link: "sokolnicki-agency",
-    title: "Sokolnicki agency",
+    title: "Sokolnicki Agency",
     header: "Website for Sokolnicki and his services",
     description: "It is fully written by me from a PSD file delivered by his designer. I assisted with a designer in some changes in design. A website is fast and very modern.",
     img: "https://wallpaperscraft.com/image/mountains_sky_bali_sunrise_kintamani_indonesia_95497_1920x1080.jpg",
-    category: "website",
+    category: "Website",
     technologies: [
-      "HTML5", "CSS3", "SASS", "Javascript", "ES6", "Gulp"
+      "HTML5/CSS3", "Javascript/ES6", "SCSS preprocessor", "Gulp task manager"
     ]
   },
   {
     id: 5,
-    link: "radek-wawrzyk",
+    link: "my-portfolio",
     title: "Radek Wawrzyk Portfolio",
     header: "Radek Wawrzyk Portfolio",
     description: "My personal website ",
     img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
-    category: "app",
+    category: "Application",
     technologies: [
-      "HTML5", "CSS3", "SASS", "Javascript", "ES6", "Vue.js", "Vuex", "Vue router", "Webpack", "Firebase"
+      "HTML5/CSS3", "Javascript/ES6", "SCSS preprocessor", "Vue.js + Vue-Router", "Vuex state management", "Webpack bundler", "Firebase realtime database"
     ]
   },
   {
@@ -66,9 +66,9 @@ export const data = [
     header: "Simple application which shows you actually weather",
     description: "Application which shows you weather based on your localisation.",
     img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
-    category: "app",
+    category: "Application",
     technologies: [
-      "HTML5", "CSS3", "Javascript", "ES6", "React.js", "API"
+      "HTML5/CSS3", "Javascript/ES6", "React.js library", "Open Weather Map API"
     ]
   },
   {
@@ -78,9 +78,9 @@ export const data = [
     header: "Rework of existing service ",
     description: "Design rework of existing service for internet fake-company in-game ETS2.",
     img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
-    category: "website",
+    category: "Website",
     technologies: [
-      "HTML5", "CSS3", "LESS", "Javascript", "jQuery"
+      "HTML5/CSS3", "Javascript", "jQuery library", "LESS preprocessor", "PHP basic template"
     ]
   },
   {
@@ -90,9 +90,9 @@ export const data = [
     header: "Page based on PSD file",
     description: "Page based on PSD file",
     img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
-    category: "app",
+    category: "Website",
     technologies: [
-      "HTML5", "CSS3", "SASS", "Javascript", "ES6", "Gulp"
+      "HTML5/CSS3", "Javascript/ES6", "SCSS preprocessor", "Gulp task manager"
     ]
   }
 ];
