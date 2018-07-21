b9df320a7a68716e1909539973915617677375c8
Radek <radek511@op.pl>
2018-07-21 18:31:34 +0200

    Edit - Home component text

--- src/Components/Home.vue
+++ src/Components/Home.vue
@@ -3,7 +3,7 @@
     <main id="home">
       <header class="home-header">
         <h1 class="home-header-title">
-          Radek Wawrzyk Junior Front End Developer
+          Radek Wawrzyk Front End Developer
         </h1>
         <p class="home-header-description">
           I'm available to <router-link to="/Contact" class="home-header-description-link" aria-label="hire me" title="hire me">hire.</router-link>
