9aad32efa20a3ce1a518eb6d2ad512b8d55d7cc4
Radek <radek511@op.pl>
2018-07-22 20:53:24 +0200

    Edit - PortfolioItemPage component with styles
    Add new images

--- src/Components/Portfolio/PortfolioItemPage.vue
+++ src/Components/Portfolio/PortfolioItemPage.vue
@@ -1,27 +1,37 @@
 <template>
-  <main class="project">
-    <header class="project-header">
-      <h1 class="project-header-title">{{item.title}}</h1>
-      <p class="project-header-subtitle">{{item.category}}</p>
-    </header>
-    <p>{{item.description}}</p>
-    <section class="project-info">
-      <h2 class="project-info-title">{{item.header}}</h2>
-      <div class="technology">
-        <h2 class="technology-title">Technology</h2>
-        <ul class="technology-list">
-          <li class="technology-item" v-for="(technology, index) in item.technologies" :key="index">
-            {{technology}}
-          </li>
-        </ul>
+  <div class="project">
+    <header class="project-header" :style="{backgroundImage: 'url('+ item.img +')'}">
+      <div class="project-header-description" >
+        <h1 class="project-header-title">{{item.title}}</h1>
+        <p class="project-header-subtitle">Category - {{item.category}}</p>
+        <p class="project-header-subtitle">
+          <a href="#">
+            Visit live
+            <svg enable-background="new 0 0 54.971 54.971" version="1.1" viewBox="0 0 54.971 54.971" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+            <path d="m51.173 3.801c-5.068-5.068-13.315-5.066-18.384 0l-9.192 9.192c-0.781 0.781-0.781 2.047 0 2.828s2.047 0.781 2.828 0l9.192-9.192c1.691-1.69 3.951-2.622 6.363-2.622 2.413 0 4.673 0.932 6.364 2.623s2.623 3.951 2.623 6.364c0 2.412-0.932 4.672-2.623 6.363l-12.019 12.022c-3.51 3.508-9.219 3.508-12.729 0-0.781-0.781-2.047-0.781-2.828 0s-0.781 2.048 0 2.828c2.534 2.534 5.863 3.801 9.192 3.801s6.658-1.267 9.192-3.801l12.021-12.021c2.447-2.446 3.795-5.711 3.795-9.192 0-3.482-1.348-6.746-3.795-9.193z"/>
+            <path d="m27.132 40.57l-7.778 7.778c-1.691 1.691-3.951 2.623-6.364 2.623-2.412 0-4.673-0.932-6.364-2.623-3.509-3.509-3.509-9.219 0-12.728l11.314-11.314c1.691-1.69 3.951-2.622 6.364-2.622 2.412 0 4.672 0.932 6.363 2.622 0.781 0.781 2.047 0.781 2.828 0s0.781-2.047 0-2.828c-5.067-5.067-13.314-5.068-18.384 0l-11.314 11.315c-2.446 2.446-3.794 5.711-3.794 9.192 0 3.48 1.348 6.745 3.795 9.191 2.446 2.447 5.711 3.795 9.191 3.795 3.481 0 6.746-1.348 9.192-3.795l7.778-7.778c0.781-0.781 0.781-2.047 0-2.828s-2.046-0.781-2.827 0z"/>
+          </svg>
+          </a>
+        </p>
       </div>
-    </section>
-    <section class="project-description">
-      <img src="https://trionndesign.com/images/work/development/berkley/slide1.jpg" width="100%">
-      <p>{{item.description}}</p>
-    </section>
-    <Footer></Footer>
-  </main>
+    </header>
+    <main id="project">
+      <section class="project-info">
+        <h2 class="project-info-title">{{item.header}}</h2>
+        <div class="technology">
+          <h2 class="technology-title">Technology</h2>
+          <ul class="technology-list">
+            <li class="technology-item" v-for="(technology, index) in item.technologies" :key="index">
+              {{technology}}
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section class="project-description">
+      </section>
+      <Footer></Footer>
+    </main>
+  </div>
 </template>
 
 <script>
--- src/Data/data.js
+++ src/Data/data.js
@@ -1,95 +1,83 @@
 export const data = [
   {
     id: 1,
-    link: "vue-todolist",
-    title: "Vue TodoList",
-    header: "ToDoList written on vue.js, where you can add your own tasks. ",
-    description: "Basically, simple to-do-list written on vue.js - you can easily add important things that you have to do in the future.",
-    img: "http://www.wallpapers4u.org/wp-content/uploads/puppy_couple_sunset_nature_play_kids_1194_1920x1080.jpg",
-    category: "Application",
-    technologies: [
-      "HTML5/CSS3", "Javascript/ES6", "Vue.js + Vue-Router", "Vuex state management"
-    ]
-  },
-  {
-    id: 2,
     link: "vue-shop",
     title: "Vue Shop",
     header: "Shop aplication based on Vue.js, REST API & Laravel",
     description: "Shop aplication based on Vue.js, REST API & Laravel",
-    img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
+    img: "http://www.image-share.com/upload/3794/154.jpg",
     category: "Application",
     technologies: [
       "HTML5/CSS3", "Javascript/ES6", "SCSS preprocessor", "Vue.js framework + Vue-Router", "Vuex state management", "JWT authorization", "REST API architecture", "Webpack module bundler",
     ]
   },
   {
-    id: 3,
+    id: 2,
     link: "bossbus",
     title: "Bossbus",
     header: "Website of a company which rents cars for specified amounts of time",
     description: "Website had to be simple, but effective. In cooperation with my graphic designer, we have created modern, simple and good looking website perfect for our client and his business. Cars are the main target which is very visible for every client.",
-    img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
+    img: "http://www.image-share.com/upload/3794/159.jpg",
     category: "Website",
     technologies: [
       "HTML5/CSS3", "Javascript/jQuery", "LESS preprocessor", "Owl Carousel library", "AOS.js animation library"
     ]
   },
   {
-    id: 4,
+    id: 3,
     link: "sokolnicki-agency",
     title: "Sokolnicki Agency",
     header: "Website for Sokolnicki and his services",
     description: "It is fully written by me from a PSD file delivered by his designer. I assisted with a designer in some changes in design. A website is fast and very modern.",
-    img: "https://wallpaperscraft.com/image/mountains_sky_bali_sunrise_kintamani_indonesia_95497_1920x1080.jpg",
+    img: "http://www.image-share.com/upload/3794/158.jpg",
     category: "Website",
     technologies: [
       "HTML5/CSS3", "Javascript/ES6", "SCSS preprocessor", "Gulp task manager"
     ]
   },
   {
-    id: 5,
+    id: 4,
     link: "my-portfolio",
     title: "Radek Wawrzyk Portfolio",
     header: "Radek Wawrzyk Portfolio",
     description: "My personal website ",
-    img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
+    img: "http://www.image-share.com/upload/3794/157.jpg",
     category: "Application",
     technologies: [
       "HTML5/CSS3", "Javascript/ES6", "SCSS preprocessor", "Vue.js + Vue-Router", "Vuex state management", "Webpack bundler", "Firebase realtime database"
     ]
   },
   {
-    id: 6,
+    id: 5,
     link: "forecast",
     title: "Forecast",
     header: "Simple application which shows you actually weather",
     description: "Application which shows you weather based on your localisation.",
-    img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
+    img: "http://www.image-share.com/upload/3794/155.jpg",
     category: "Application",
     technologies: [
       "HTML5/CSS3", "Javascript/ES6", "React.js library", "Open Weather Map API"
     ]
   },
   {
-    id: 7,
+    id: 6,
     link: "mp-transporte",
     title: "MP Transporte",
     header: "Rework of existing service ",
     description: "Design rework of existing service for internet fake-company in-game ETS2.",
-    img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
+    img: "http://www.image-share.com/upload/3794/156.jpg",
     category: "Website",
     technologies: [
       "HTML5/CSS3", "Javascript", "jQuery library", "LESS preprocessor", "PHP basic template"
     ]
   },
   {
-    id: 8,
+    id: 7,
     link: "cersoir",
     title: "Cersoir",
     header: "Page based on PSD file",
     description: "Page based on PSD file",
-    img: "https://wallpaperscraft.com/image/bali_balinese_balinese_cat_breed_color_96025_1920x1080.jpg",
+    img: "http://www.image-share.com/upload/3794/154.jpg",
     category: "Website",
     technologies: [
       "HTML5/CSS3", "Javascript/ES6", "SCSS preprocessor", "Gulp task manager"
