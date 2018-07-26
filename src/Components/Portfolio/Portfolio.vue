3c4e6696fe533c22f9b2d3fd2377474b697edb54
Radek <radek511@op.pl>
2018-07-26 20:20:45 +0200

    Add - Active class for filters buttons

--- src/Components/Portfolio/Portfolio.vue
+++ src/Components/Portfolio/Portfolio.vue
@@ -10,22 +10,19 @@
         </p>
       </header>
       <ul class="portfolio-filter">
-        <li class="portfolio-filter-item">
-          <button class="portfolio-filter-item-button" @click="filter" data-category="default">All</button>
+        <li class="portfolio-filter-item" @click="activeButton='all'">
+          <button class="portfolio-filter-item-button" :class="[ activeButton === 'all' ? 'active' : '']" @click="filter" data-category="default">All</button>
         </li>
-        <li class="portfolio-filter-item">
-          <button class="portfolio-filter-item-button" @click="filter" data-category="Website">Website</button>
+        <li class="portfolio-filter-item" @click="activeButton='website'">
+          <button class="portfolio-filter-item-button" :class="[ activeButton === 'website' ? 'active' : '']" @click="filter" data-category="Website" >Website</button>
         </li>
-        <li class="portfolio-filter-item">
-          <button class="portfolio-filter-item-button" @click="filter" data-category="Application">Application</button>
+        <li class="portfolio-filter-item" @click="activeButton='app'">
+          <button class="portfolio-filter-item-button" :class="[ activeButton === 'app' ? 'active' : '']" @click="filter" data-category="Application">Application</button>
         </li>
       </ul>
       <transition-group class="portfolio-list" tag="ul" name="moving-fade">
         <PortfolioItem v-bind:portfolioItem="portfolioItem" v-for="portfolioItem in portfolioItems" :key="portfolioItem.id"></PortfolioItem>
       </transition-group>
-      <transition name="fade-left">
-        <router-view></router-view>
-      </transition>
     </main>
     <Footer></Footer>
   </div>
@@ -43,11 +40,16 @@ export default {
     PortfolioItem,
     Footer
   },
+  data() {
+    return {
+      activeButton: "all"
+    }
+  },
   computed: {
     ...mapGetters(["portfolioItems"])
   },
   methods: {
-    filter(event) {
+    filter(event, xd) {
       this.$store.dispatch("filtration", event.target.getAttribute("data-category"));
     }
   }
