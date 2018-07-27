cb345f0de8ad61d7d4364d0becc6279a1a5f0d3b
Radek <radek511@op.pl>
2018-07-27 19:34:34 +0200

    Add - Imitation of logo

--- src/Components/Navigation.vue
+++ src/Components/Navigation.vue
@@ -1,7 +1,14 @@
 <template>
   <nav class="navigation">
     <figure class="navigation-logo">
-      <router-link aria-label="Homepage" title="Homepage" to="/"><img src="https://blue-shark.co.uk/wp-content/uploads/2016/05/icon-web_dev.png" alt="logo"/></router-link>
+      <router-link aria-label="Homepage" title="Homepage" to="/">
+        <figcaption class="desktop">
+          <span>Radek-</span>Wawrzyk
+        </figcaption>
+        <figcaption class="mobile">
+          <span>R</span>w
+        </figcaption>
+      </router-link>
     </figure>
     <button 
       class="navigation-button" v-bind:class="{'active': actvieClass}"  v-bind:aria-expanded="actvieClass" 
@@ -11,10 +18,10 @@
       <span></span>
     </button>
     <ul class="navigation-menu" v-bind:class="{'active': actvieClass}" id="menu">
-      <li v-on:click="toggleNav" ><router-link to='/'>Home</router-link></li>
-      <li v-on:click="toggleNav" ><router-link aria-label="Portfolio subpage" title="Portfolio subpage" to="/portfolio">Portfolio</router-link></li>
-      <li v-on:click="toggleNav" ><router-link aria-label="About subpage" title="About subpage" to="/about">About</router-link></li>
-      <li v-on:click="toggleNav" ><router-link aria-label="Contact subpage" title="Contact subpage" to="/contact">Contact</router-link></li>
+      <li @click="toggleNav" ><router-link to='/'>Home</router-link></li>
+      <li @click="toggleNav" ><router-link aria-label="Portfolio subpage" title="Portfolio subpage" to="/portfolio">Portfolio</router-link></li>
+      <li @click="toggleNav" ><router-link aria-label="About subpage" title="About subpage" to="/about">About</router-link></li>
+      <li @click="toggleNav" ><router-link aria-label="Contact subpage" title="Contact subpage" to="/contact">Contact</router-link></li>
     </ul>
   </nav>
 </template>
