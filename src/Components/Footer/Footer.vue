de36cb73b2d0d7577b7f3538174470f12053ee3a
Radek <radek511@op.pl>
2018-08-18 02:55:29 +0200

    Replace - New logotype

--- src/Components/Footer/Footer.vue
+++ src/Components/Footer/Footer.vue
@@ -56,12 +56,7 @@
     </ul>
     <figure class="footer-logo">
       <router-link aria-label="Homepage" title="Homepage" to="/">
-        <figcaption class="desktop">
-          <span>Radek-</span>Wawrzyk
-        </figcaption>
-        <figcaption class="mobile">
-          <span>R</span>w
-        </figcaption>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 795 800"><g id="main"><rect class="cls-1" width="250" height="800"/><rect class="cls-1" x="546.37" y="490.87" width="280.81" height="363.27" transform="translate(-349.24 333.49) rotate(-30)"/><rect class="cls-1" x="500.57" y="412.75" width="370.77" height="218.55" transform="translate(-281.04 419.4) rotate(-37.1)"/><rect class="cls-1" y="397" width="340" height="403"/><rect class="cls-1" width="795" height="198"/><rect class="cls-1" x="515" width="280" height="397"/><rect class="cls-1" y="397" width="595" height="200"/><rect class="cls-1" x="551" y="659" width="244" height="141"/></g></svg>
       </router-link>
     </figure>
     <p class="footer-singature">© 2018 Radek Wawrzyk</p>
