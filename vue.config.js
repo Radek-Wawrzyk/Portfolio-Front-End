09660ed05dd3f76eb3675b285dea4798cc860428
Radek <radek511@op.pl>
2019-02-24 21:12:20 +0000

    Add - VeeValidate library and global SCSS variables file

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `@import "@/Styles/Components/Colors.scss";`
+      }
+    }
+  }
 }
