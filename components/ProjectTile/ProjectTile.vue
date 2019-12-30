ea8c23eba55a5b95a7a62680bc5457a1638e4761
Radek <radek511@op.pl>
2019-12-30 01:36:45 +0100

    Simply portfolio grid

---
+++ components/ProjectTile/ProjectTile.vue
@@ -0,0 +1,40 @@
+<template>
+  <li class="project-tile">
+    <nuxt-link
+      :to="`projects/${project.slug}`"
+      class="project-tile__link"
+      :title="project.name"
+      :aria-label="project.name"
+    >
+      <figure class="project-tile__img-wrapper">
+        <img
+          class="project-tile__img"
+          :alt="project.name"
+          :src="project.imageURL"
+        />
+      </figure>
+      <div class="project-tile__content">
+        <h3 class="project-tile__heading">
+          {{ project.name }} — {{ project.shortDescription }}
+        </h3>
+        <p class="project-tile__sub-heading">
+          {{ project.keys }}
+        </p>
+      </div>
+    </nuxt-link>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'ProjectTile',
+  props: {
+    project: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" src="./ProjectTile.scss" />
